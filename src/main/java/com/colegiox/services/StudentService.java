8db2c54d978eae766d4883fcdd517dca77146a55
Paulo Henrique <paulo.13.henrique800@gmail.com>
2023-11-18 20:09:50 -0300

    criando camada de serviços para aluno

---
+++ src/main/java/com/colegiox/services/StudentService.java
@@ -0,0 +1,37 @@
+package com.colegiox.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.colegiox.entities.Student;
+import com.colegiox.repository.StudentRepository;
+
+@Service
+public class StudentService {
+	
+	@Autowired
+	private StudentRepository repository;
+	
+	
+	public List<Student>findAll(){
+		
+		return repository.findAll();
+	}
+	public Student findById(Integer id) {
+		return repository.findById(id).get();
+	}
+	public void delete(Integer id) {
+		repository.deleteById(id);
+	}
+	public Student insert(Student obj) {
+		return repository.save(obj);
+	}
+	public Student update(Integer id, Student obj) {
+		findById(id);
+		
+		return repository.save(obj);
+	}
+
+}
